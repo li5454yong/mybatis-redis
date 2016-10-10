69bcae4a89dca7e44144530f6fb9e917f80aa52e
lxg <283056051@qq.com>
2016-10-10 21:08:51 +0800

    初次提交

---
+++ src/main/java/com/lxg/service/impl/LxgServiceImpl.java
@@ -0,0 +1,43 @@
+package com.lxg.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.lxg.dao.LxgDao;
+import com.lxg.entity.TestEntity;
+import com.lxg.service.LxgService;
+
+/**
+ * @author lxg
+ *
+ * 2016年9月4日下午4:18:01
+ */
+@Service
+public class LxgServiceImpl implements LxgService {
+
+	@Resource
+	private LxgDao lxgDao;
+	
+	public void update(Map<String,Object> param){
+		lxgDao.update(param);
+	}
+	
+	public List<TestEntity> getLxg(){
+		return lxgDao.getLxg();
+	}
+
+	@Override
+	public void del(Map<String, Object> param) {
+		lxgDao.del(param);
+		
+	}
+
+	@Override
+	public void save(TestEntity t) {
+		lxgDao.save(t);
+	}
+}
